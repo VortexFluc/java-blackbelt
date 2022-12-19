78a3b6f29ea718ad9842b20e9a4618456104b3a7
Andrey Mihaylin <look2009new@gmail.com>
2022-12-19 15:57:15 +0300

    Multithreading. Synchronized method.

---
+++ src/multithreading/Ex11.java
@@ -0,0 +1,30 @@
+package multithreading;
+
+public class Ex11 {
+    static int counter = 0;
+    public static synchronized void increment() {counter++;}
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new R());
+        Thread thread2 = new Thread(new R());
+
+        for (int i = 0; i < 10; i++) {
+            thread1.start();
+            thread2.start();
+
+            thread1.join();
+            thread2.join();
+            System.out.println(counter);
+        }
+    }
+}
+
+class R implements Runnable {
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000000; i++) {
+            Ex11.increment();
+        }
+    }
+}
