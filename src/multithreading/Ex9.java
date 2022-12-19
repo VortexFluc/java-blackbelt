3f8a958820fdf407c61e14363c46f524732bbbca
Andrey Mihaylin <look2009new@gmail.com>
2022-12-19 15:15:47 +0300

    Multithreading sleep, join, name and order.

---
+++ src/multithreading/Ex9.java
@@ -0,0 +1,28 @@
+package multithreading;
+
+public class Ex9 {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Main begins");
+        Thread thread = new Thread(new Worker());
+        System.out.println("Before start + " + thread.getState());
+        thread.start();
+        System.out.println("After start + " + thread.getState());
+        thread.join(4000);
+        System.out.println("After join + " + thread.getState());
+
+        System.out.println("Method main ends");
+    }
+}
+
+class Worker implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Work begins");
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Work ends");
+    }
+}
